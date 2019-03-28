2ea39d7349d4a977fef5321bce978ada24b3b274
doncho_new <doncho_new@CUSTOM-08.finitegroup.local>
2019-03-28 15:59:11 +0200

    References #6541 - delete example migration

--- config/database.php
+++ config/database.php
@@ -51,12 +51,8 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => false,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'engine' => null
         ],
 
         'pgsql' => [

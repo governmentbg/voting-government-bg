63c44215a97bc3c7370209bec84387d0adaeb463
doncho_new <doncho_new@CUSTOM-08.finitegroup.local>
2019-03-28 14:05:19 +0200

    Fixes #6541 - Внедряване на MVC Framework Laravel

---
+++ app/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        
+    ];
+}
---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+update () {
+    # Turn on maintenance mode
+    # php artisan down
+
+    /usr/local/bin/composer install --no-dev --optimize-autoloader
+
+    # Cache boost configuration and routes
+    php artisan cache:clear
+
+    # Clear and cache config
+    php artisan config:clear
+    php artisan config:cache
+
+    php artisan route:clear
+    php artisan view:clear
+
+    # Sync database changes
+    php artisan migrate --force
+
+    # Restart workers
+    #php artisan queue:restart
+
+    # Install npm packages & compile scss files
+    #npm install
+    #npm run production
+
+    # Turn off maintenance mode
+    # php artisan up
+}
+
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+git fetch --tags
+git checkout $1
+
+if ! [[ $1 =~ ^v?[0-9\.]+$ ]]
+then
+    if [[ $(git merge origin/$1) != 'Already up-to-date.' ]]
+    then
+        update
+    fi
+else
+    update
+fi

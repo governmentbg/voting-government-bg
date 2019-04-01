4f2a85e4cd4e23ed57ba85a4bd07969c9ec4cf27
Kaloyan Dimov <kaloyan@finite-soft.com>
2019-04-01 12:18:53 +0300

    References #6548 Api controller, WIP

---
+++ app/Http/Controllers/Api/VotingTourController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\VotingTour;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\QueryException;
+use App\Http\Controllers\ApiController;
+
+class VotingTourController extends ApiController
+{
+    public function addVotingTour(Request $request)
+    {
+       // to be implemented
+    }
+}

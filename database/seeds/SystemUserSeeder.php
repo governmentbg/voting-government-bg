80e50574b53ee2cebdb10f68cba947a274fbaae0
Doncho Toromanov <doncho@finite-soft.com>
2019-04-05 11:08:50 +0300

    References #6543 - check if system user already exists

--- database/seeds/SystemUserSeeder.php
+++ database/seeds/SystemUserSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class SystemUserSeeder extends Seeder
 {
@@ -12,13 +13,20 @@ class SystemUserSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class)->create([
-            'username' => config('auth.system.user'),
-            'password' => Hash::make(str_random(60)),
-            'active' => 1,
-            'first_name' => config('auth.system.user'),
-            'last_name' => config('auth.system.user'),
-            'email' => config('auth.system.user'),
-        ]);
+        if(!User::where('username', config('auth.system.user'))->first()){
+            factory(\App\User::class)->create([
+                'username' => config('auth.system.user'),
+                'password' => Hash::make(str_random(60)),
+                'active' => 1,
+                'first_name' => config('auth.system.user'),
+                'last_name' => config('auth.system.user'),
+                'email' => config('auth.system.user'),
+            ]);
+        }
+        else{
+            if (isset($this->command)) {
+                $this->command->warn("System user already exists!");
+            }
+        }
     }
 }

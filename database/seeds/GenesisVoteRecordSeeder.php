6c9330dcba38b40e3080bec1048027a5b98a65c5
Kaloyan Dimov <kaloyan@finite-soft.com>
2019-04-05 14:34:24 +0300

    References #6552 Add validation if env hash variable is set

--- database/seeds/GenesisVoteRecordSeeder.php
+++ database/seeds/GenesisVoteRecordSeeder.php
@@ -14,19 +14,23 @@ class GenesisVoteRecordSeeder extends Seeder
     public function run()
     {
         if (!Vote::where('voter_id', 0)->where('voting_tour_id', 0)->first()){
-            Schema::disableForeignKeyConstraints();
+            if (config('database.INITIAL_HASH')) {
+                Schema::disableForeignKeyConstraints();
 
-            // Genesis block for votes
-            factory(Vote::class)->create([
-                'vote_time' => date('Y-m-d H:i:s'),
-                'voter_id' => 0,
-                'voting_tour_id' => 0,
-                'vote_data' => 'genesis',
-                'tour_status' => 99,
-                'prev_hash' => hash('sha256', config('database.INITIAL_HASH'))
-            ]);
+                // Genesis block for votes
+                factory(Vote::class)->create([
+                    'vote_time' => date('Y-m-d H:i:s'),
+                    'voter_id' => 0,
+                    'voting_tour_id' => 0,
+                    'vote_data' => 'genesis',
+                    'tour_status' => 99,
+                    'prev_hash' => hash('sha256', config('database.INITIAL_HASH'))
+                ]);
 
-            Schema::enableForeignKeyConstraints();
+                Schema::enableForeignKeyConstraints();
+            } else {
+                $this->command->warn("Initial hash not set for environment!");
+            }
         } else {
             if (isset($this->command)) {
                 $this->command->warn("Genesis block already created!");

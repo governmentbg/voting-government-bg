bdb549ec1aae3a78f66f96fcad09aa0613e51e18
Antonia Pepeliankova <antonia@finite-soft.com>
2020-01-30 19:46:50 +0200

    References #8148 - set registration status changes; X-Frame-Options header fix

--- app/Http/Middleware/HttpHeaders.php
+++ app/Http/Middleware/HttpHeaders.php
@@ -16,7 +16,7 @@ class HttpHeaders
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        
+
         // $contentSecurityPolicyOptions = "default-src 'none';";
         // $contentSecurityPolicyOptions = "form-action 'self';";
         // $response->headers->set('Content-Security-Policy-Report-Only', $contentSecurityPolicyOptions);
@@ -25,7 +25,7 @@ class HttpHeaders
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains'); // https://scotthelme.co.uk/hsts-the-missing-link-in-tls/
         $response->headers->set('X-Content-Type-Options', 'nosniff'); // https://scotthelme.co.uk/hardening-your-http-response-headers/#x-content-type-options
         $response->headers->set('X-XSS-Protection', '1; mode=block'); // https://scotthelme.co.uk/hardening-your-http-response-headers/#x-content-type-options
-        $response->headers->set('X-Frame-Options', 'SAMEORIGIN; allow-from https://www.schulzhotels.com/'); // https://scotthelme.co.uk/hardening-your-http-response-headers/#x-frame-options
+        $response->headers->set('X-Frame-Options', 'SAMEORIGIN'); // https://scotthelme.co.uk/hardening-your-http-response-headers/#x-frame-options
         //$response->headers->set('Feature-Policy', "vibrate 'self'; usermedia *;"); // https://scotthelme.co.uk/a-new-security-header-feature-policy
 
         return $response;

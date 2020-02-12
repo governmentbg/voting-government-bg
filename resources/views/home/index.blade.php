4771a3394a48839501838d363beaf8c3245ca172
Kaloyan Dimov <kaloyan@finite-soft.com>
2020-02-12 10:45:12 +0200

    References #8357 open file in new window

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -17,10 +17,10 @@
                 {{ __('custom.front_text') }}<br><br>
                 {{ __('custom.front_text_2') }}<br>
                 <span class="c-darkBlue font-weight-bold">{{__('custom.information')}} </span>{{ __('custom.front_info') }} <span class="font-weight-bold">
-                    <a href="{{ Storage::url('downloads/Инфо за СРГО.pdf') }}">ТУК</a>.
+                    <a href="{{ Storage::url('downloads/Инфо за СРГО.pdf') }}" target="_blank">ТУК</a>.
                 </span><br>
                 {{ __('custom.front_terms_start') }} <span class="c-darkBlue font-weight-bold">{{__('custom.terms')}} </span> {{ __('custom.front_terms_end') }} <span class="font-weight-bold">
-                    <a href="{{ Storage::url('downloads/Условия за ползване Платформата за избор на СРГО.pdf') }}">ТУК</a>.
+                    <a href="{{ Storage::url('downloads/Условия за ползване Платформата за избор на СРГО.pdf') }}" target="_blank">ТУК</a>.
                 </span><br>
                 <span class="c-darkBlue font-weight-bold">{{ __('custom.coming_soon') }}</span><br>
                 {{ __('custom.created_for') }}
